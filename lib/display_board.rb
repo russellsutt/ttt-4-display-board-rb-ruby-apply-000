e9bf81e4b0bddd8313ca1a8c2cb1b6747c3409dd
Russell Sutter <russell.sutter@gmail.com>
2020-06-17 22:49:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -23,4 +23,61 @@ def display_board(board)
   puts "-----------"
   puts " #{board[0]} | #{board[0]} | #{board[0]} "
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+end
+
 display_board(board)
