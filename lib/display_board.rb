fc24e45889f068d7d5713e66aa151fcae90fd346
Russell Sutter <russell.sutter@gmail.com>
2020-06-17 22:35:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 board = ["X","X","X"," "," "," "," "," "," "]
 
 def display_board(board)
-  puts 
-  puts ""   |   |   
-  puts "   |   |  "
-  puts "-----------"
-  puts "   |   |  "
+  puts
+  puts
+  puts
+  puts
+  puts
 end
 
 display_board(board)
